2c11e7caf79d0ea00bb6d4736e0ea8ba0f70b501
hudhamalick <noorulcse@gmail.com>
2022-05-26 09:56:51 +0530

    change

--- src/global.js
+++ src/global.js
@@ -1,1 +1,5 @@
-export const API="https://620aeaff92946600171c5cbc.mockapi.io"
+//export const API="https://620aeaff92946600171c5cbc.mockapi.io"
+
+export const API="https://hudha-app.herokuapp.com"
+
+//export const API = "https://movie-list-app-react-nodejs.herokuapp.com"
